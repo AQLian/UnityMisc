59b25fb483d3f1505323ff3dc1b660422227b089
AQ <aq@qq.com>
2025-05-21 13:01:43 +0800

    1

--- Scripts/Editor/EditorUtility.cs
+++ Scripts/Editor/EditorUtility.cs
@@ -7,56 +7,54 @@ namespace Scripts.Editor
 {
     public class EditorUtility
     {
-        
-        
-    public static void GetAllPaths(GameObject prefab, List<string> paths)
-    {
-        if (paths == null)
+        public static void GetAllPaths(GameObject prefab, List<string> paths)
         {
-            throw new System.ArgumentException($"Provided {nameof(paths)} is null.");
-        }
-
-        paths.Clear();
-        CollectInnerPaths(prefab.transform, "", paths);
-    }
-
-    public static List<string> GetAllPaths(GameObject prefab)
-    {
-        if (prefab == null)
-        {
-            Debug.LogWarning($"Provided {nameof(prefab)} is null.");
-            return new List<string>();
-        }
-
-        List<string> paths = new List<string>();
-        CollectInnerPaths(prefab.transform, "", paths);
-        return paths;
-    }
-
-    private static void CollectInnerPaths(Transform current, string parentPath, List<string> paths)
-    {
-        string currentPath = string.IsNullOrEmpty(parentPath) ? current.name : $"{parentPath}/{current.name}";
-        paths.Add(currentPath);
-
-        foreach (Transform child in current)
-        {
-            CollectInnerPaths(child, currentPath, paths);
-        }
-    }
-
-    private static GameObject[] GetPrefabAssetsWithName(string name)
-    {
-        string[] guids = AssetDatabase.FindAssets(name + " t:Prefab");
-        if (guids.Length > 0)
-        {
-            return guids.Select(g => 
+            if (paths == null)
             {
-                string path = AssetDatabase.GUIDToAssetPath(g);
-                return AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            }).ToArray();
+                throw new System.ArgumentException($"Provided {nameof(paths)} is null.");
+            }
+
+            paths.Clear();
+            CollectInnerPaths(prefab.transform, "", paths);
         }
 
-        return null;
-    }
+        public static List<string> GetAllPaths(GameObject prefab)
+        {
+            if (prefab == null)
+            {
+                Debug.LogWarning($"Provided {nameof(prefab)} is null.");
+                return new List<string>();
+            }
+
+            List<string> paths = new List<string>();
+            CollectInnerPaths(prefab.transform, "", paths);
+            return paths;
+        }
+
+        private static void CollectInnerPaths(Transform current, string parentPath, List<string> paths)
+        {
+            string currentPath = string.IsNullOrEmpty(parentPath) ? current.name : $"{parentPath}/{current.name}";
+            paths.Add(currentPath);
+
+            foreach (Transform child in current)
+            {
+                CollectInnerPaths(child, currentPath, paths);
+            }
+        }
+
+        private static GameObject[] GetPrefabAssetsWithName(string name)
+        {
+            string[] guids = AssetDatabase.FindAssets(name + " t:Prefab");
+            if (guids.Length > 0)
+            {
+                return guids.Select(g => 
+                {
+                    string path = AssetDatabase.GUIDToAssetPath(g);
+                    return AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                }).ToArray();
+            }
+
+            return null;
+        }
     }
 }
