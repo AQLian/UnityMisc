ae9b168155af94d990f995ae7602ff5b2b8584c8
AQ <aq@qq.com>
2026-03-13 23:19:30 +0800

    enhanced scroller

---
+++ Scripts/EnhanceScrollView/MyUGUIEnhanceItem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class MyUGUIEnhanceItem : EnhanceItem
+{
+    private Button uButton;
+    private RawImage rawImage;
+    public Action onClickAction;
+
+    protected override void OnStart()
+    {
+        rawImage = GetComponent<RawImage>();
+        uButton = GetComponent<Button>();
+        onClickAction = OnClickUGUIButton;
+    }
+
+    private void OnClickUGUIButton()
+    {
+        OnClickEnhanceItem();
+    }
+
+    // Set the item "depth" 2d or 3d
+    protected override void SetItemDepth(float depthCurveValue, int depthFactor, float itemCount)
+    {
+        var newDepth = (int)(depthCurveValue * itemCount);
+        this.transform.SetSiblingIndex(newDepth);
+    }
+
+    public override void SetSelectState(bool isCenter)
+    {
+        if (rawImage == null)
+            rawImage = GetComponent<RawImage>();
+        
+        rawImage.color = isCenter ? Color.white : Color.gray;
+    }
+}
---
+++ Scripts/EnhanceScrollView/UDragEnhanceView.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UDragEnhanceView : EventTrigger
+{
+    private EnhanceScrollView enhanceScrollView;
+    public void SetScrollView(EnhanceScrollView view)
+    {
+        enhanceScrollView = view;
+    }
+    
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+
+        var dragging = eventData.dragging;
+        if (dragging)
+        {
+            return;
+        }
+        
+        if (this.TryGetComponent<MyUGUIEnhanceItem>(out var inst))
+        {
+            inst.onClickAction?.Invoke();
+        }
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        if (enhanceScrollView != null)
+            enhanceScrollView.OnDragEnhanceViewMove(eventData);
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        if (enhanceScrollView != null)
+            enhanceScrollView.OnDragEnhanceViewEnd();
+    }
+}
