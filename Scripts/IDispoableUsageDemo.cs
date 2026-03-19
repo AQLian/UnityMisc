5a18ac399dcaa0adf9d78d56410a00d48a57515f
AQ <aq@qq.com>
2026-03-19 22:44:59 +0800

    idisposable usage small demo

---
+++ Scripts/IDispoableUsageDemo.cs
@@ -0,0 +1,58 @@
+
+public class NormalReport : IDisposable
+{
+    public static int MaxFieldCount { get; private set; } = 10;
+    protected List<string> reserveFields = new List<string>();
+    protected bool _disposed;
+
+    public NormalReport(List<string> reserves)
+    {
+        reserves.ForEach(AddReserveField);
+        OnStartRecord();
+    }
+
+    public void AddReserveField(string field)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+        if (reserveFields.Count > MaxFieldCount)
+        {
+            return;
+        }
+        reserveFields.Add(field);
+    }
+
+    protected virtual void OnStartRecord() { }
+
+    protected virtual void OnStopRecord() { }
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+        _disposed = true;
+        
+    }
+}
+
+public class ReportWithElapsedTime : NormalReport
+{
+    private StopWatch stopWatch;
+
+    public ReportWithElapsedTime(List<string> reserves):base(reserves){}
+
+    protected override void OnStartRecord()
+    {
+        base.OnStartRecord();
+        stopWatch = StopWatch.StartNew();
+    }
+
+    protected override void OnStopRecord()
+    {
+        stopWatch.Stop();
+        var time = stopWatch.ElapsedMilliseconds / 1000f;
+        AddReserveField(time.ToString());
+        base.OnStopRecord();
+    }
+}
